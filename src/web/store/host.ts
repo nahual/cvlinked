f4762d3154f7bada27e8c27989dfe0ce6049a26e
Christian L. Angelone <christian.angelone@wildlifestudios.com>
2021-06-13 21:49:03 -0300

    Adds vercel url

--- src/web/store/host.ts
+++ src/web/store/host.ts
@@ -1,2 +1,3 @@
 // export default 'https://cvlinked.herokuapp.com';
-export default 'http://localhost:3000';
+// export default 'http://localhost:3000';
+export default 'https://cvlinked.vercel.app';
